0ba965f2ad840ce1114f664fc4b37b0fca556f8c
Rama1433 <ramacicarelli2001@gmail.com>
2022-10-02 21:42:03 -0300

    re-hice el tp

---
+++ src/middlewares/adminCheck.js
@@ -0,0 +1,12 @@
+//const { VERSION } = require("ejs");
+const admin = ['ada', 'vim', 'tim', 'greta'];
+
+module.exports = (req, res, next) => {
+    if(admin.includes(req.query.user.toLowerCase())){
+        next()
+    }else{
+        res.redirect('/login?error=true');
+    }
+}
+
+
